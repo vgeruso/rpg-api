121a92b9141ce741230d30677954da89de676bd8
VictorGeruso <victor.geruso@gmail.com>
2019-08-24 21:03:34 -0300

    [UPD] adjustment setup script

--- src/db/scripts/setup.sh
+++ src/db/scripts/setup.sh
@@ -4,7 +4,7 @@ cd config
 touch database.js
 echo -e "const mongoose = require('mongoose');\n" >> database.js
 echo -e "module.exports = mongoose.connect(" >> database.js
-echo -e "   'mongodb+srv://rpg:p3GWunjc5vL0KYVe@rpgdb-lb7ko.mongodb.net/'+process.env.MONGOLAB_DB+'?retryWrites=true&w=majority'," >> database.js
+echo -e "   'mongodb+srv://rpg:p3GWunjc5vL0KYVe@rpgdb-lb7ko.mongodb.net:27017/'+process.env.MONGOLAB_DB+'?retryWrites=true&w=majority'," >> database.js
 echo -e "   {" >> database.js
 echo -e "       useNewUrlParser: true" >> database.js
 echo -e "   }" >> database.js
