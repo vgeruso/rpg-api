e12dd74bdf345f76bc6ed05b210676d2b01bf082
geruso <victor.geruso@gmail.com>
2019-10-17 22:15:41 -0300

    [UPD] Adjustment of routes

--- src/app/routes.js
+++ src/app/routes.js
@@ -2,14 +2,14 @@ const express = require('express');
 const routes = express.Router();
 
 // Controllers
-// Game
 const User = require('./controllers/game/UserController');
-// Breed
 const Breed = require('./controllers/roles/BreedController');
-// Class
 const Class = require('./controllers/roles/ClassController');
+const Arm = require('./controllers/equipments/ArmController');
+const DefenseItem = require('./controllers/equipments/DefenseItemController');
 
 // Routes and operations
+// Game
 // Users
 routes.post('/user', User.store);
 routes.get('/users', User.index);
@@ -17,14 +17,43 @@ routes.get('/user/:id', User.show);
 routes.put('/user/:id', User.update);
 routes.delete('/user/:id', User.destroy);
 
-// Breed
-routes.post('/breed', Breed.store);
-routes.get('/breeds', Breed.index);
-routes.get('/breed/:id', Breed.show);
-
+// Roles
 // Class
 routes.post('/class', Class.store);
 routes.get('/classes', Class.index);
 routes.get('/class/:id', Class.show);
+routes.put('/class/:id', Class.update);
+routes.delete('/class/:id', Class.destroy);
+// Breed
+routes.post('/breed', Breed.store);
+routes.get('/breeds', Breed.index);
+routes.get('/breed/:id', Breed.show);
+routes.put('/breed/:id', Breed.update);
+routes.delete('/breed/:id', Breed.destroy);
+
+// Equipments
+// Arm
+routes.post('/arm', Arm.store);
+routes.get('/arms', Arm.index);
+routes.get('/arm/:id', Arm.show);
+routes.put('/arm/:id', Arm.update);
+routes.delete('/arm/:id', Arm.destroy);
+// Defense Item
+// TODO test of URIs
+routes.post('/defenseItem', DefenseItem.store);
+routes.get('/defenseItems', DefenseItem.index);
+routes.get('/defenseItem/:id', DefenseItem.show);
+routes.put('/defenseItem/:id', DefenseItem.update);
+routes.delete('/defenseItem/:id', DefenseItem.destroy);
+// TODO General Item
+// TODO Magic
+
+// Attributes
+// TODO Charisma
+// TODO Constituition
+// TODO Dexterity
+// TODO Intelligence
+// TODO Strength
+// TODO Winsdom
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const app = require('./app');
 
+const PORT = 3000
+
 // Initialization of the server in port 3000
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Server on');
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server on in PORT ${PORT}`);
 });
