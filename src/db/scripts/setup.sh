1e1082d232ca18720a7d6f934bfc9b6ecfc5e76c
Victor Geruso Gomes <victor.geruso@gmail.com>
2019-11-29 07:58:39 -0300

    [UPD] Adjustment of the setup and doc travis

--- src/db/scripts/setup.sh
+++ src/db/scripts/setup.sh
@@ -6,7 +6,8 @@ echo -e "const mongoose = require('mongoose');\n" >> database.js
 echo -e "module.exports = mongoose.connect(" >> database.js
 echo -e "   'mongodb://localhost:27017/'+process.env.MONGOLAB_DB," >> database.js
 echo -e "   {" >> database.js
-echo -e "       useNewUrlParser: true" >> database.js
+echo -e "       useNewUrlParser: true," >> database.js
+echo -e "       useUnifiedTopology: true" >> database.js
 echo -e "   }" >> database.js
 echo -e ");" >> database.js
 echo "Setup Ok"
